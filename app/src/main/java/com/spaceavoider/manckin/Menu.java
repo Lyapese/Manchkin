3ba822734c692f82bbb018eca3dcfbccc3d3652b
RiopyX <danilbar245@gmail.com>
2022-03-15 12:40:53 +0300

    11

---
+++ app/src/main/java/com/spaceavoider/manckin/Menu.java
@@ -0,0 +1,11 @@
+package com.spaceavoider.manckin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import android.widget.TextView;
+
+public class Menu {
+}
