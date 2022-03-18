4fffe44f98b27fd206f7a240e5647b10be2c8348
Lyapese <Kamilla.bu4@gmail.com>
2022-03-18 16:16:10 +0300

    2

--- app/src/main/java/com/spaceavoider/manckin/Menu.java
+++ app/src/main/java/com/spaceavoider/manckin/Menu.java
@@ -3,7 +3,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,28 +15,23 @@ public class Menu extends AppCompatActivity implements View.OnClickListener  {
         button = findViewById(R.id.button);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
-        button.setOnClickListener(this);
     }
     @SuppressLint("NonConstantResourceId")
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.button:
                 Intent intent = new Intent(this, Activity.class);
                 startActivity(intent);
                 break;
             case R.id.button2:
-                Toast toast = Toast.makeText(getApplicationContext(),"В разработке", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER, 0, -10);
+                Toast toast = Toast.makeText(getApplicationContext(), "Hello Android!",Toast.LENGTH_LONG);
                 toast.show();
-                break;
             case R.id.button3:
                 Intent homeIntent = new Intent(Intent.ACTION_MAIN);
                 homeIntent.addCategory( Intent.CATEGORY_HOME );
                 homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(homeIntent);
-                break;
-            default:
                 break;
         }
     }
