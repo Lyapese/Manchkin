069ce46092d98c4e822a2dbe5cdb019d2a306818
Lyapese <Kamilla.bu4@gmail.com>
2022-03-30 21:22:48 +0300

    3

--- app/src/main/java/com/spaceavoider/manckin/Ecologic.java
+++ app/src/main/java/com/spaceavoider/manckin/Ecologic.java
@@ -13,7 +13,8 @@ import java.text.DecimalFormat;
 public class Ecologic extends AppCompatActivity {
     EditText EditText1, EditText2;
     TextView ScoreOut;
-    Float a1, b1;
+    double a1;
+    double b1;
     String x;
     float k;
 
@@ -32,20 +33,25 @@ public class Ecologic extends AppCompatActivity {
         b = EditText2.getText().toString();
         a1 = Float.parseFloat(a);
         b1 = Float.parseFloat(b);
+        if (a1 == 0){
+            a1 = 1.0;
+        }
+        if (b1 == 0) {
+            b1 = 1.0;
+        }
         if (a1 < b1){
-            k = (a1/b1)*(a1+b1);
+            k = (float) ((a1/b1)*(a1+b1));
             DecimalFormat decimalFormat = new DecimalFormat("#.#");
             x = decimalFormat.format(k);
         }
         if (a1 > b1){
-            k = (b1/a1)*(a1+b1);
+            k = (float) ((b1/a1)*(a1+b1));
             DecimalFormat decimalFormat = new DecimalFormat("#.#");
             x = decimalFormat.format(k);
         }
         if (a1 == b1){
-            k = a1+b1;
-            DecimalFormat decimalFormat = new DecimalFormat("#.#");
-            x = decimalFormat.format(k);
+            k = (int) (a1+b1);
+            x = Integer.toString((int) k);
         }
     ScoreOut.setText(x);
     }
