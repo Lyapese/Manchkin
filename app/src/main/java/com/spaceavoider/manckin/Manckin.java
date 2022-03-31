fcc24a11f90e4a20068b9fb59f0e4926807512bd
Lyapese <Kamilla.bu4@gmail.com>
2022-03-31 21:52:34 +0300

    1

--- app/src/main/java/com/spaceavoider/manckin/Manckin.java
+++ app/src/main/java/com/spaceavoider/manckin/Manckin.java
@@ -1,6 +1,5 @@
 package com.spaceavoider.manckin;
 
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,6 +7,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,48 +20,32 @@ public class Manckin extends AppCompatActivity {
     private int b = 1;
     private int c = 1;
     private int d = 1;
+    EditText editTextTextPersonName1 = findViewById(R.id.editTextTextPersonName1);
+    EditText editTextTextPersonName2 = findViewById(R.id.editTextTextPersonName2);
+    EditText editTextTextPersonName3 = findViewById(R.id.editTextTextPersonName3);
+    EditText editTextTextPersonName4 = findViewById(R.id.editTextTextPersonName4);
+    EditText editTextTextPersonName5 = findViewById(R.id.editTextTextPersonName5);
+    EditText editTextTextPersonName6 = findViewById(R.id.editTextTextPersonName6);
+    String Name1;
+    String Name2;
+    String Name3;
+    String Name4;
+    String Name5;
+    String Name6;
     SharedPreferences myPreferences;
-    EditText editTextTextPersonName1, editTextTextPersonName2, editTextTextPersonName3, editTextTextPersonName4, editTextTextPersonName5, editTextTextPersonName6;
-
 
     @SuppressLint("ResourceAsColor")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextTextPersonName1 = findViewById(R.id.editTextTextPersonName1);
-        editTextTextPersonName2 = findViewById(R.id.editTextTextPersonName2);
-        editTextTextPersonName3 = findViewById(R.id.editTextTextPersonName3);
-        editTextTextPersonName4 = findViewById(R.id.editTextTextPersonName4);
-        editTextTextPersonName5 = findViewById(R.id.editTextTextPersonName5);
-        editTextTextPersonName6 = findViewById(R.id.editTextTextPersonName6);
         myPreferences = PreferenceManager.getDefaultSharedPreferences(Manckin.this);
-        String Name1 = "";
-        String Name2 = "";
-        String Name3 = "";
-        String Name4 = "";
-        String Name5 = "";
-        String Name6 = "";
-        editTextTextPersonName1.setText(Name1);
-        editTextTextPersonName2.setText(Name2);
-        editTextTextPersonName3.setText(Name3);
-        editTextTextPersonName4.setText(Name4);
-        editTextTextPersonName5.setText(Name5);
-        editTextTextPersonName6.setText(Name6);
-        SharedPreferences.Editor myEditor = myPreferences.edit();
-        myEditor.putString("Name1", String.valueOf(editTextTextPersonName1));
-        myEditor.putString("Name2", String.valueOf(editTextTextPersonName2));
-        myEditor.putString("Name3", String.valueOf(editTextTextPersonName3));
-        myEditor.putString("Name4", String.valueOf(editTextTextPersonName4));
-        myEditor.putString("Name5", String.valueOf(editTextTextPersonName5));
-        myEditor.putString("Name6", String.valueOf(editTextTextPersonName6));
-        myEditor.apply();
-        Name1 = myPreferences.getString("Name1", "");
-        Name2 = myPreferences.getString("Name2", "");
-        Name3 = myPreferences.getString("Name3", "");
-        Name4 = myPreferences.getString("Name4", "");
-        Name5 = myPreferences.getString("Name5", "");
-        Name6 = myPreferences.getString("Name6", "");
+        Name1 = myPreferences.getString("Name1", Name1);
+        Name2 = myPreferences.getString("Name2", Name2);
+        Name3 = myPreferences.getString("Name3", Name3);
+        Name4 = myPreferences.getString("Name4", Name4);
+        Name5 = myPreferences.getString("Name5", Name5);
+        Name6 = myPreferences.getString("Name6", Name6);
         editTextTextPersonName1.setText(Name1);
         editTextTextPersonName2.setText(Name2);
         editTextTextPersonName3.setText(Name3);
@@ -179,6 +164,116 @@ public class Manckin extends AppCompatActivity {
                 view6.setText(String.valueOf(d));
             } else {
                 view6.setText(String.valueOf(d));
+            }
+        });
+
+        editTextTextPersonName1.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Name1 = s.toString();
+                SharedPreferences.Editor myEditor = myPreferences.edit();
+                myEditor.putString("Name1", Name1);
+                myEditor.apply();
+
+            }
+        });
+        editTextTextPersonName2.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Name2 = s.toString();
+                SharedPreferences.Editor myEditor = myPreferences.edit();
+                myEditor.putString("Name2", Name2);
+                myEditor.apply();
+            }
+        });
+        editTextTextPersonName3.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Name3 = s.toString();
+                SharedPreferences.Editor myEditor = myPreferences.edit();
+                myEditor.putString("Name3", Name3);
+                myEditor.apply();
+            }
+        });
+        editTextTextPersonName4.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Name4 = s.toString();
+                SharedPreferences.Editor myEditor = myPreferences.edit();
+                myEditor.putString("Name4", Name4);
+                myEditor.apply();
+            }
+        });
+        editTextTextPersonName5.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Name5 = s.toString();
+                SharedPreferences.Editor myEditor = myPreferences.edit();
+                myEditor.putString("Name5", Name5);
+                myEditor.apply();
+            }
+        });
+        editTextTextPersonName6.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Name6 = s.toString();
+                SharedPreferences.Editor myEditor = myPreferences.edit();
+                myEditor.putString("Name6", Name6);
+                myEditor.apply();
             }
         });
 
