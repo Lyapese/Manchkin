ee8a592ee42965f129ee57a8265c98e751031327
Lyapese <Kamilla.bu4@gmail.com>
2022-04-01 10:12:47 +0300

    2

--- app/src/main/java/com/spaceavoider/manckin/Menu.java
+++ app/src/main/java/com/spaceavoider/manckin/Menu.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 public class Menu extends AppCompatActivity implements View.OnClickListener  {
     @Override
