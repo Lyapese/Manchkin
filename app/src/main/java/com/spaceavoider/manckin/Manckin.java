7a0fe679a2d00784e22931399ef6b7de793e75d3
Lyapese <Kamilla.bu4@gmail.com>
2022-04-22 23:22:57 +0300

    111

--- app/src/main/java/com/spaceavoider/manckin/Manckin.java
+++ app/src/main/java/com/spaceavoider/manckin/Manckin.java
@@ -60,116 +60,142 @@ public class Manckin extends AppCompatActivity {
         button_back = findViewById(R.id.button_back1);
 
         TextView view1 = findViewById(R.id.view1);
-
-        findViewById(R.id.buttonMin1).setOnClickListener( v -> {
-            if (x > 1) {
-                x--;
-                view1.setText(String.valueOf(x));
-            } else {
-                view1.setText(String.valueOf(x));
-            }
-        });
-        findViewById(R.id.buttonMax1).setOnClickListener( v -> {
-            if (x < 10) {
-                x++;
-                view1.setText(String.valueOf(x));
-            } else {
-                view1.setText(String.valueOf(x));
-            }
-        });
-
         TextView view2 = findViewById(R.id.view2);
-
-        findViewById(R.id.buttonMin2).setOnClickListener( v -> {
-            if (y > 1) {
-                y--;
-                view2.setText(String.valueOf(y));
-            } else {
-                view2.setText(String.valueOf(y));
-            }
-        });
-        findViewById(R.id.buttonMax2).setOnClickListener( v -> {
-            if (y < 10) {
-                y++;
-                view2.setText(String.valueOf(y));
-            } else {
-                view2.setText(String.valueOf(y));
-            }
-        });
-
         TextView view3 = findViewById(R.id.view3);
-
-        findViewById(R.id.buttonMin3).setOnClickListener( v -> {
-            if (a > 1) {
-                a--;
-                view3.setText(String.valueOf(a));
-            } else {
-                view3.setText(String.valueOf(a));
-            }
-        });
-        findViewById(R.id.buttonMax3).setOnClickListener( v -> {
-            if (a < 10) {
-                a++;
-                view3.setText(String.valueOf(a));
-            } else {
-                view3.setText(String.valueOf(a));
-            }
-        });
-
         TextView view4 = findViewById(R.id.view4);
-
-        findViewById(R.id.buttonMin4).setOnClickListener( v -> {
-            if (b > 1) {
-                b--;
-                view4.setText(String.valueOf(b));
-            } else {
-                view4.setText(String.valueOf(b));
-            }
-        });
-        findViewById(R.id.buttonMax4).setOnClickListener( v -> {
-            if (b < 10) {
-                b++;
-                view4.setText(String.valueOf(b));
-            } else {
-                view4.setText(String.valueOf(b));
-            }
-        });
-
         TextView view5 = findViewById(R.id.view5);
-
-        findViewById(R.id.buttonMin5).setOnClickListener( v -> {
-            if (c > 1) {
-                c--;
-                view5.setText(String.valueOf(c));
-            } else {
-                view5.setText(String.valueOf(c));
-            }
-        });
-        findViewById(R.id.buttonMax5).setOnClickListener( v -> {
-            if (c < 10) {
-                c++;
-                view5.setText(String.valueOf(c));
-            } else {
-                view5.setText(String.valueOf(c));
-            }
-        });
-
         TextView view6 = findViewById(R.id.view6);
 
-        findViewById(R.id.buttonMin6).setOnClickListener( v -> {
-            if (d > 1) {
-                d--;
-                view6.setText(String.valueOf(d));
-            } else {
-                view6.setText(String.valueOf(d));
+        findViewById(R.id.buttonMin1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (x > 1) {
+                    x--;
+                    view1.setText(String.valueOf(x));
+                } else {
+                    view1.setText(String.valueOf(x));
+                }
             }
         });
-        findViewById(R.id.buttonMax6).setOnClickListener( v -> {
-            if (d < 10) {
-                d++;
-                view6.setText(String.valueOf(d));
-            } else {
-                view6.setText(String.valueOf(d));
+        findViewById(R.id.buttonMax1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (x < 10) {
+                    x++;
+                    view1.setText(String.valueOf(x));
+                } else {
+                    view1.setText(String.valueOf(x));
+                }
+            }
+        });
+        findViewById(R.id.buttonMin2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (y > 1) {
+                    y--;
+                    view2.setText(String.valueOf(y));
+                } else {
+                    view2.setText(String.valueOf(y));
+                }
+            }
+        });
+        findViewById(R.id.buttonMax2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (y < 10) {
+                    y++;
+                    view2.setText(String.valueOf(y));
+                } else {
+                    view2.setText(String.valueOf(y));
+                }
+            }
+        });
+        findViewById(R.id.buttonMin3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (a > 1) {
+                    a--;
+                    view3.setText(String.valueOf(a));
+                } else {
+                    view3.setText(String.valueOf(a));
+                }
+            }
+        });
+        findViewById(R.id.buttonMax3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (a < 10) {
+                    a++;
+                    view3.setText(String.valueOf(a));
+                } else {
+                    view3.setText(String.valueOf(a));
+                }
+            }
+        });
+        findViewById(R.id.buttonMin4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b > 1) {
+                    b--;
+                    view4.setText(String.valueOf(b));
+                } else {
+                    view4.setText(String.valueOf(b));
+                }
+            }
+        });
+        findViewById(R.id.buttonMax4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b < 10) {
+                    b++;
+                    view4.setText(String.valueOf(b));
+                } else {
+                    view4.setText(String.valueOf(b));
+                }
+            }
+        });
+        findViewById(R.id.buttonMin5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (c > 1) {
+                    c--;
+                    view5.setText(String.valueOf(c));
+                } else {
+                    view5.setText(String.valueOf(c));
+                }
+            }
+        });
+        findViewById(R.id.buttonMax5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (c < 10) {
+                    c++;
+                    view5.setText(String.valueOf(c));
+                } else {
+                    view5.setText(String.valueOf(c));
+                }
+            }
+        });
+        findViewById(R.id.buttonMin6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (d > 1) {
+                    d--;
+                    view6.setText(String.valueOf(d));
+                } else {
+                    view6.setText(String.valueOf(d));
+                }
+            }
+        });
+        findViewById(R.id.buttonMax6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (d < 10) {
+                    d++;
+                    view6.setText(String.valueOf(d));
+                } else {
+                    view6.setText(String.valueOf(d));
+                }
             }
         });
 
